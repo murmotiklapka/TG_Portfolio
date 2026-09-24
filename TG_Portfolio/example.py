27f962f96a31b24cf0abc9bb1a531ebfd4fdaf8b
Ilya <murmotiklapka@gmail.com>
2026-09-24 20:53:43 +0200

    Add files via upload

---
+++ TG_Portfolio/example.py
@@ -0,0 +1,16 @@
+eat = [
+    ('burger', '$20', '1000'),
+    ('fries', '$5', '500'),
+    ('pizza', '$15', '2000'),
+    ('chokalate', '$10', '600')
+]
+print(eat)
+
+students = [('18', 22), ('17', 20), ('16', 25)]
+print(sorted(students, key=lambda x: x[1]))
+
+states = {
+    (40.7128, 74.0060): "New York"
+
+}
+print(states[(40.7128, 74.0060)])
